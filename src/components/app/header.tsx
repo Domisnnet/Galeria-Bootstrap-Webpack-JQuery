bf36be6b4072b0008aeefd3e8edaafe2e99c89aa
Dominique Gonçalves <dominique.goncalves61@gmail.com>
2025-11-28 19:36:52 +0000

    na verdade, a logo e os botões estão colados na página

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -59,7 +59,7 @@ export function Header({
   };
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4">
       <div className="container flex h-24 items-center justify-between gap-4">
         <div className="flex items-center gap-2">
           <BrainCircuit className="h-12 w-12 text-primary" />
@@ -114,7 +114,7 @@ export function Header({
             </Form>
           </div>
         </div>
-        <div className="flex items-center gap-2" />
+        <div className="w-12" />
       </div>
     </header>
   );
